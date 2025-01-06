ddf28250b7fdf85b255f8085e5a3cd36e738adb9
MaximZharinoff <minimum.cold1991@gmail.com>
2025-01-06 10:52:21 +0700

    create logger

---
+++ src/main/java/ru/netology/moneytransferservice/NtlgCourseworkMoneyTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.moneytransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtlgCourseworkMoneyTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtlgCourseworkMoneyTransferServiceApplication.class, args);
+    }
+
+}
