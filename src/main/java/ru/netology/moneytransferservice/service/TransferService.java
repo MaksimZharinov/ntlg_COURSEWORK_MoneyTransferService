2e0679346c9811d44381a687df388fd594ea16b4
MaximZharinoff <minimum.cold1991@gmail.com>
2025-01-08 13:11:54 +0700

    create service

---
+++ src/main/java/ru/netology/moneytransferservice/service/TransferService.java
@@ -0,0 +1,69 @@
+package ru.netology.moneytransferservice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.netology.moneytransferservice.exception.TransferException;
+import ru.netology.moneytransferservice.model.ConfirmationRequest;
+import ru.netology.moneytransferservice.model.TransferRecord;
+import ru.netology.moneytransferservice.model.TransferRequest;
+import ru.netology.moneytransferservice.model.TransferResponse;
+import ru.netology.moneytransferservice.repository.TransferRepository;
+import ru.netology.moneytransferservice.util.FileLogger;
+
+import java.io.IOException;
+
+@Service
+public class TransferService {
+
+    @Value("${application.fee}")
+    private double fee;
+    private final TransferRepository transferRepository;
+    private final FileLogger fileLogger;
+
+    public TransferService(TransferRepository transferRepository, FileLogger fileLogger) {
+        this.transferRepository = transferRepository;
+        this.fileLogger = fileLogger;
+    }
+
+    public TransferResponse execute(TransferRequest request)
+            throws TransferException, IOException {
+        // Логика перевода средств
+        String result = "SUCCESS";
+        double commission = calculateCommission(request.getAmount());
+
+        fileLogger.logTransfer(
+                request.getCardFrom(),
+                request.getCardTo(),
+                request.getAmount(),
+                commission,
+                result
+        );
+
+        transferRepository.save(request, result);
+
+        return new TransferResponse(result);
+    }
+
+    public boolean confirmOperation(ConfirmationRequest request)
+            throws TransferException, IOException {
+        // Получаем запись о переводе по operationId
+        TransferRecord record = transferRepository.findByOperationId(request.getOperationId())
+                .orElseThrow(() -> new TransferException(404, "Operation not found"));
+
+        // Проверяем код подтверждения
+        if (!request.getCode().equals(record.getVerificationCode())) {
+            throw new TransferException(401, "Invalid verification code");
+        }
+
+        // Подтверждаем операцию
+        record.setConfirmed(true);
+        transferRepository.update(record);
+
+        return true;
+    }
+
+
+    private double calculateCommission(double amount) {
+        return amount * fee;
+    }
+}
