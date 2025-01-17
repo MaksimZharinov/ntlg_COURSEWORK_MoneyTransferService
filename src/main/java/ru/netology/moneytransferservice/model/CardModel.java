8d29d631a34fde25029f3b1b44a541178986e783
MaximZharinoff <minimum.cold1991@gmail.com>
2025-01-17 12:10:11 +0700

    add Card model

---
+++ src/main/java/ru/netology/moneytransferservice/model/CardModel.java
@@ -0,0 +1,10 @@
+package ru.netology.moneytransferservice.model;
+
+import lombok.Data;
+
+@Data
+public class CardModel {
+    private String cardNumber;
+    private String cardValidTill;
+    private String cardCVV;
+}
